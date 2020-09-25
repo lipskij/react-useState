0964c33336e6ccef64f1be9a96b960284ff1f9ce
lipskij <emil.lipskij@gmail.com>
2020-09-25 13:56:00 +0300

    submit

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -2,22 +2,30 @@ import React, { useState } from "react";
 
 const Input = () => {
   const [name, setInput] = useState("");
-  const [istrue, setIstrue] = useState(false);
-
-  // function handleClick() {
-  //   return setIstrue(true)
-  // }
-
+  // make change only after button click
   return (
     <div>
       <form>
         <label>
           Name
-          <input type="text" placeholder="name" onChange={e => setInput(e.target.value)}/>
+          <input
+            type="text"
+            placeholder="Place your name"
+            onChange={e => setInput(e.target.value)}
+          />
         </label>
-        <button type="submit" onClick={(event) => {event.preventDefault(); setIstrue(true)}}>Click</button>
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            console.log("clicked");
+            setInput("")
+          }}
+        >
+          Click
+        </button>
       </form>
-      {istrue ? <p> Your name: {name}</p> : ""}
+      <h2>Your name: {name}</h2>
     </div>
   );
 };
